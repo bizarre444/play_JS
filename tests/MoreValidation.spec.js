5935d00c817531dfe816fdcfb23c503d229db6f4
Bizarre <la_insolence@mail.ru>
2022-07-14 11:15:43 +0300

    screenshot test

--- tests/MoreValidation.spec.js
+++ tests/MoreValidation.spec.js
@@ -21,6 +21,18 @@ test("Popup validations", async({ page }) => {
     await framesPage.locator("li a[href*='lifetime-access']:visible").click();
     const textCheck = await framesPage.locator(".text h2").textContent();
     console.log(textCheck.split(" ")[1]);
+});
 
-
+test("Screenshot & Visual comparition", async({ page }) => {
+    await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+    await expect(page.locator("#displayed-text")).toBeVisible();
+    await page.locator('#displayed-text').screenshot({ path: 'partialScreenshot.png' });
+    await page.locator("#hide-textbox").click();
+    await page.screenshot({ path: 'screenshot.png' });
+    await expect(page.locator("#displayed-text")).toBeHidden();
+});
+//screenshot - store -> screenshot -> 
+test.only('Visual', async({ page }) => {
+    await page.goto("https://google.com/");
+    expect(await page.screenshot()).toMatchSnapshot('landing.png');
 })
